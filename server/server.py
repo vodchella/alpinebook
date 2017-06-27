a5b7beb09f766304dc0243576cc6672baf7fdeb1
twister <twister.kz@gmail.com>
2017-06-27 12:35:21 +0600

    Динамический импорт всех модулей из директории /pkg/app

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 import importlib
+from os import path
+from glob import glob
 from pkg.app import app
 
 if __name__ == '__main__':
-    for md in ['regions', 'listeners', 'middleware', 'exceptions']:
+    for md in [path.basename(x)[:-3] for x in glob('./pkg/app/*.py') if x[-11:] != '__init__.py']:
         importlib.import_module('pkg.app.' + md)
     app.run(host='localhost', port=8000)
