a6cb8f4b49670f1716a95eac0c556be56e442d6b
twister <twister.kz@gmail.com>
2017-06-27 13:44:14 +0600

    Изменена выдаваемая информация и структура путей. Сейчас это: /regions /regions/<id> /regions/<id>/areas /regions/<id>/areas/<id>

--- server/pkg/app/routes.py
+++ server/pkg/app/routes.py
@@ -13,11 +13,11 @@ async def get_region(request, region_id: int):
     return response.json(await Executor().query_one(app.db_statements['get_region'], region_id))
 
 
-@app.route("/areas")
-async def list_regions(request):
-    return response.json(await Executor().query_all(app.db_statements['get_areas']))
+@app.route("/regions/<region_id:int>/areas")
+async def list_regions(request, region_id: int):
+    return response.json(await Executor().query_all(app.db_statements['get_areas'], region_id))
 
 
-@app.route("/areas/<area_id:int>")
-async def list_regions(request, area_id: int):
-    return response.json(await Executor().query_all(app.db_statements['get_area'], area_id))
+@app.route("/regions/<region_id:int>/areas/<area_id:int>")
+async def list_regions(request, region_id: int, area_id: int):
+    return response.json(await Executor().query_all(app.db_statements['get_area'], region_id, area_id))
--- server/pkg/postgresql/queries.py
+++ server/pkg/postgresql/queries.py
@@ -11,12 +11,7 @@ order by r.region_id
 
 SQL_GET_REGION = """
 select json_build_object('region_id', r.region_id,
-                         'region', max(r.region),
-                         'areas', array_agg(case
-                                              when a.area_id is not null then
-                                                json_build_object('area_id', a.area_id,
-                                                                  'area', a.area)
-                                            end)) as region
+                         'region', max(r.region)) as region
 from   regions r
        left join areas a  on (a.region_id = r.region_id)
 where  r.region_id = $1
@@ -27,22 +22,19 @@ SQL_GET_AREAS = """
 select json_build_object('area_id', a.area_id,
                          'area', a.area) as area
 from   areas a
-order by a.area_id
+where  a.region_id = $1
+order by a.area
 """
 
 SQL_GET_AREA = """
 select json_build_object('area_id', a.area_id,
                          'area', max(a.area),
                          'region', json_build_object('region_id', max(r.region_id),
-                                                     'region', max(r.region)),
-                         'mountains', array_agg(case
-                                                  when m.mountain_id is not null then
-                                                    json_build_object('mountain_id', m.mountain_id,
-                                                                      'mountain', m.mountain)
-                                                end)) as area
+                                                     'region', max(r.region))) as area
 from   areas a
        left  join mountains m  on (m.area_id = a.area_id)
        inner join regions   r  on (r.region_id = a.region_id)
-where  a.area_id = $1
+where  r.region_id = $1 and
+       a.area_id = $2
 group by a.area_id
 """
