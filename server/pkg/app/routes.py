927baaaf943593528b632c135f8ca8904f3ecc75
twister <twister.kz@gmail.com>
2017-06-27 13:07:31 +0600

    Отдача списка областей по адресу /areas и детализации области по адресу /areas/<id>

--- server/pkg/app/routes.py
+++ server/pkg/app/routes.py
@@ -11,3 +11,13 @@ async def list_regions(request):
 @app.route("/regions/<region_id:int>")
 async def get_region(request, region_id: int):
     return response.json(await Executor().query_one(app.db_statements['get_region'], region_id))
+
+
+@app.route("/areas")
+async def list_regions(request):
+    return response.json(await Executor().query_all(app.db_statements['get_areas']))
+
+
+@app.route("/areas/<area_id:int>")
+async def list_regions(request, area_id: int):
+    return response.json(await Executor().query_all(app.db_statements['get_area'], area_id))
--- server/pkg/postgresql/queries.py
+++ server/pkg/postgresql/queries.py
@@ -14,10 +14,32 @@ select json_build_object('region_id', r.region_id,
                          'region', max(r.region),
                          'areas', array_agg(case
                                               when a.area_id is not null then
-                                                json_build_object('area_id', a.area_id, 'area', a.area)
+                                                json_build_object('area_id', a.area_id,
+                                                                  'area', a.area)
                                             end)) as region
 from   regions r
        left join areas a  on (a.region_id = r.region_id)
 where  r.region_id = $1
 group by r.region_id
+"""
+
+SQL_GET_AREAS = """
+select json_build_object('area_id', a.area_id,
+                         'area', a.area) as area
+from   areas a
+order by a.area_id
+"""
+
+SQL_GET_AREA = """
+select json_build_object('area_id', a.area_id,
+                         'area', max(a.area),
+                         'mountains', array_agg(case
+                                                  when m.mountain_id is not null then
+                                                    json_build_object('mountain_id', m.mountain_id,
+                                                                      'area', m.mountain)
+                                                end)) as area
+from   areas a
+       left join mountains m  on (m.area_id = a.area_id)
+where  a.area_id = $1
+group by a.area_id
 """
