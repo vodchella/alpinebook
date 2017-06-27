f78b84d8e4355b470a92603ecbf5da52504a5faf
twister <twister.kz@gmail.com>
2017-06-27 13:51:05 +0600

    Явно определены http-методы для обработчиков

--- server/pkg/app/routes.py
+++ server/pkg/app/routes.py
@@ -3,21 +3,21 @@ from sanic import response
 from . import app
 
 
-@app.route("/regions")
+@app.route('/regions', methods=['GET'])
 async def list_regions(request):
     return response.json(await Executor().query_all(app.db_statements['get_regions']))
 
 
-@app.route("/regions/<region_id:int>")
+@app.route('/regions/<region_id:int>', methods=['GET'])
 async def get_region(request, region_id: int):
     return response.json(await Executor().query_one(app.db_statements['get_region'], region_id))
 
 
-@app.route("/regions/<region_id:int>/areas")
+@app.route('/regions/<region_id:int>/areas', methods=['GET'])
 async def list_regions(request, region_id: int):
     return response.json(await Executor().query_all(app.db_statements['get_areas'], region_id))
 
 
-@app.route("/regions/<region_id:int>/areas/<area_id:int>")
+@app.route('/regions/<region_id:int>/areas/<area_id:int>', methods=['GET'])
 async def list_regions(request, region_id: int, area_id: int):
     return response.json(await Executor().query_all(app.db_statements['get_area'], region_id, area_id))
