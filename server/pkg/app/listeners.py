df313f050c774f8a67a2df379894ad7f857ecda7
twister <twister.kz@gmail.com>
2017-06-27 18:38:33 +0600

    А теперь забыл удалить лишний код

--- server/pkg/app/listeners.py
+++ server/pkg/app/listeners.py
@@ -10,9 +10,8 @@ async def setup(app, loop):
                                          max_size=10,
                                          max_inactive_connection_lifetime=0,
                                          command_timeout=60)
-    async with app.pool.acquire() as conn:
-        for query in filter(lambda q: 'SQL_' == q[:4], dir(pkg.postgresql.queries)):
-            app.db_queries[query.lower()[4:]] = getattr(pkg.postgresql.queries, query)
+    for query in filter(lambda q: 'SQL_' == q[:4], dir(pkg.postgresql.queries)):
+        app.db_queries[query.lower()[4:]] = getattr(pkg.postgresql.queries, query)
 
 
 @app.listener('after_server_stop')
