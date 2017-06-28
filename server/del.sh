6c17a0cb170f9448a130fdb23680063388b8860b
twister <twister.kz@gmail.com>
2017-06-28 19:59:37 +0600

    Подкорректировал del.sh

--- server/del.sh
+++ server/del.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-find pkg -type f -regex ".*\.\(pyc\|log\)" -exec rm -rf {} \;
-find pkg -type d -name "__pycache__"  -exec rmdir {} \; 2> /dev/null
+del() {
+	find $1 -type f -regex ".*\.\(pyc\|log\)" -exec rm -rf {} \;
+	find $1 -type d -name "__pycache__"  -exec rmdir {} \; 2> /dev/null
+}
+
+del pkg
+del data
 rm -rf *.log
