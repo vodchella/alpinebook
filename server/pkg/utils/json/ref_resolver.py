16154e1bf2e3a11cf58f6e3da66f322d1aa047aa
twister <twister.kz@gmail.com>
2017-06-28 19:00:01 +0600

    Добавлена возможность резолвить ссылки на другие локальные json-схемы

--- server/pkg/utils/json/ref_resolver.py
+++ server/pkg/utils/json/ref_resolver.py
@@ -1,18 +1,36 @@
+#  Основано на https://github.com/purukaushik/ref-resolver
+
 import jsonpath_rw
 from urllib.parse import urlparse
 
 
 class SimpleRefResolver:
     _json_obj = None
+    _schemas = None
+
+    def __init__(self, schemas=None):
+        self._schemas = schemas
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
     def _resolve(self, json_obj):
         if isinstance(json_obj, dict):
             for key, value in json_obj.items():
-                if key == "$ref":
+                if key == '$ref':
                     ref_frag = urlparse(value)
-                    ref_path_expr = "$" + ".".join(ref_frag.fragment.split("/"))
+
+                    if ref_frag.scheme == 'schemas' and self._schemas:
+                        json_dump = self._schemas[ref_frag.netloc]
+                    else:
+                        json_dump = self._json_obj
+
+                    ref_path_expr = '$' + '.'.join(ref_frag.fragment.split('/'))
                     path_expression = jsonpath_rw.parse(ref_path_expr)
-                    list_of_values = [match.value for match in path_expression.find(self._json_obj)]
+                    list_of_values = [match.value for match in path_expression.find(json_dump)]
                     if len(list_of_values) > 0:
                         resolution = list_of_values[0]
                         return resolution
