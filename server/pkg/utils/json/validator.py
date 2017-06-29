bf62aadd73a0e9278c4a1fa5491cce9454474fb7
twister <twister.kz@gmail.com>
2017-06-29 15:37:56 +0600

    Небольшой рефакторинг

--- server/pkg/utils/json/validator.py
+++ server/pkg/utils/json/validator.py
@@ -9,6 +9,9 @@ from pkg.utils.errors import response_error, ERROR_SCHEMA_VALIDATION
 class Validator:
     def validate(self, json_object, schema_name):
         try:
-            jsonschema.validate(json_object, schemas[schema_name])
+            self.validate_and_raise_error(json_object, schema_name)
         except ValidationError as e:
             return response_error(ERROR_SCHEMA_VALIDATION, e.message)
+
+    def validate_and_raise_error(self, json_object, schema_name):
+        jsonschema.validate(json_object, schemas[schema_name])
