10ff4056cbcd362e4bdd46edf3a4182f9c85ef81
twister <twister.kz@gmail.com>
2017-07-13 13:35:32 +0600

    Переименовываю методы

--- server/pkg/app/routes.py
+++ server/pkg/app/routes.py
@@ -36,39 +36,39 @@ async def get_region(request, region_id: int):
 
 @app.route('/regions/<region_id:int>/areas',
            methods=['GET'])
-async def list_regions(request, region_id: int):
+async def list_areas(request, region_id: int):
     return response.json(await Executor(request).query_all_json(app.db_queries['get_areas'],
                                                          region_id))
 
 
 @app.route('/regions/<region_id:int>/areas/<area_id:int>', methods=['GET'])
-async def list_regions(request, region_id: int, area_id: int):
+async def get_area(request, region_id: int, area_id: int):
     return response.json(await Executor(request).query_one_json(app.db_queries['get_area'],
                                                          region_id, area_id))
 
 
 @app.route('/regions/<region_id:int>/areas/<area_id:int>/mountains', methods=['GET'])
-async def list_regions(request, region_id: int, area_id: int):
+async def list_mountains(request, region_id: int, area_id: int):
     return response.json(await Executor(request).query_all_json(app.db_queries['get_mountains'],
                                                          region_id, area_id))
 
 
 @app.route('/regions/<region_id:int>/areas/<area_id:int>/mountains/<mountain_id:int>',
            methods=['GET'])
-async def list_regions(request, region_id: int, area_id: int, mountain_id: int):
+async def get_mountain(request, region_id: int, area_id: int, mountain_id: int):
     return response.json(await Executor(request).query_one_json(app.db_queries['get_mountain'],
                                                          region_id, area_id, mountain_id))
 
 
 @app.route('/regions/<region_id:int>/areas/<area_id:int>/mountains/<mountain_id:int>/routes',
            methods=['GET'])
-async def list_regions(request, region_id: int, area_id: int, mountain_id: int):
+async def list_routes(request, region_id: int, area_id: int, mountain_id: int):
     return response.json(await Executor(request).query_all_json(app.db_queries['get_routes'],
                                                          region_id, area_id, mountain_id))
 
 
 @app.route('/regions/<region_id:int>/areas/<area_id:int>/mountains/<mountain_id:int>/routes/<route_id:int>',
            methods=['GET'])
-async def list_regions(request, region_id: int, area_id: int, mountain_id: int, route_id: int):
+async def get_route(request, region_id: int, area_id: int, mountain_id: int, route_id: int):
     return response.json(await Executor(request).query_one_json(app.db_queries['get_route'],
                                                          region_id, area_id, mountain_id, route_id))
