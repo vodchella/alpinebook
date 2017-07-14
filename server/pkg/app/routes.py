da3d340b32ff35258141fab8fce096dba5fd1621
twister <twister.kz@gmail.com>
2017-07-14 19:20:16 +0600

    Добавлен post-метод /summits/<id>

--- server/pkg/app/routes.py
+++ server/pkg/app/routes.py
@@ -1,8 +1,10 @@
 from pkg.postgresql.executor import Executor
 from pkg.postgresql.builder import QueryBuilder
 from pkg.utils.decorators.validate_request import validate_request
-from pkg.utils.errors import response_error, ERROR_UNIQUE_VIOLATION
+from pkg.utils.errors import response_error, get_raised_error
+from pkg.utils.errors import ERROR_INTERNAL_EXCEPTION, ERROR_DATABASE_EXCEPTION, ERROR_UNIQUE_VIOLATION
 from asyncpg.exceptions import UniqueViolationError
+from asyncpg.exceptions._base import PostgresError
 from sanic import response
 from . import app
 
@@ -12,15 +14,32 @@ async def get_summits(request, alpinist_id: int):
     return response.json(await Executor(request).query_all_json(app.db_queries['get_summits'], alpinist_id))
 
 
+@app.route('/summits/<summit_id:int>', methods=['POST'])
+@validate_request('alpinist_summits')
+async def update_summit(request, summit_id: int):
+    try:
+        query_data = QueryBuilder('alpinist_summits').generate_update(request.json)
+        result = await Executor(request).query_one(query_data['sql'], summit_id, *query_data['values'])
+        return response.json({'updated': result})
+    except PostgresError as e:
+        return response_error(ERROR_DATABASE_EXCEPTION, str(e))
+    except:
+        return response_error(ERROR_INTERNAL_EXCEPTION, get_raised_error())
+
+
 @app.route('/summits', methods=['POST'])
 @validate_request('alpinist_summits')
 async def insert_summit(request):
-    query_data = QueryBuilder('alpinist_summits').generate_insert(request.json)
     try:
+        query_data = QueryBuilder('alpinist_summits').generate_insert(request.json)
         result = await Executor(request).query_one(query_data['sql'], *query_data['values'])
         return response.json({'new_id': result})
     except UniqueViolationError:
         return response_error(ERROR_UNIQUE_VIOLATION, 'Запись с таким идентификатором уже существует')
+    except PostgresError as e:
+        return response_error(ERROR_DATABASE_EXCEPTION, str(e))
+    except:
+        return response_error(ERROR_INTERNAL_EXCEPTION, get_raised_error())
 
 
 #
