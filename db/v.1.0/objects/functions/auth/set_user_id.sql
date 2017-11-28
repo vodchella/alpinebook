782b13d22b11d86e6a152796104df56b356ffcfe
twister <twister.kz@gmail.com>
2017-11-28 15:30:49 +0600

    Из функции set_user_id() в целях ускорения убрана проверка на активность и существование пользователя

--- db/v.1.0/objects/functions/auth/set_user_id.sql
+++ db/v.1.0/objects/functions/auth/set_user_id.sql
@@ -1,20 +1,9 @@
 CREATE OR REPLACE FUNCTION auth.set_user_id(id integer)
   RETURNS boolean AS
 $BODY$
-declare
-  res bool;
 begin
-  select count(*) > 0
-  into   res
-  from   auth.users u
-  where  u.user_id = id and
-         u.active_bool;
-
-  if res then
-    perform set_config('alpinebook.current_user_id', id::text, false);
-  end if;
-  
-  return res;
+  perform set_config('alpinebook.current_user_id', id::text, false);
+  return true;
 end 
 $BODY$
   LANGUAGE plpgsql;
