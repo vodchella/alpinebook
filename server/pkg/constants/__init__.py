ab7dda54dfdcb2bfabeb55775a65fee28b1b59e6
twister <twister.kz@gmail.com>
2017-12-15 19:20:37 +0600

    В строку с версией добавлен текущий git-коммит

--- server/pkg/constants/__init__.py
+++ server/pkg/constants/__init__.py
@@ -1,4 +1,10 @@
+from pkg.utils.git import get_top_commit
+
 DEBUG = True
 APPLICATION_NAME = 'AlpineBook'
 VERSION = '0.01'
-APPLICATION_VERSION = APPLICATION_NAME + ' ' + VERSION
+APPLICATION_VERSION = APPLICATION_NAME + ' v' + VERSION
+
+git_commit = get_top_commit()
+if git_commit:
+    APPLICATION_VERSION += '.' + git_commit
--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
+import logging
 import importlib
 from os import path
 from glob import glob
 from pkg.app import app
+from pkg.constants import APPLICATION_VERSION
 
 
 if __name__ == '__main__':
+    logging.getLogger('alpinebook').info(APPLICATION_VERSION + ' started!')
     for md in [path.basename(x)[:-3] for x in glob('./pkg/app/*.py') if x[-11:] != '__init__.py']:
         importlib.import_module('pkg.app.' + md)
     app.run(host='localhost', port=8000)
