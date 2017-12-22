5c4d9357b5bbd356095d1471c1c2719d87325dd8
twister <twister.kz@gmail.com>
2017-12-22 17:33:35 +0600

    Добавил тестовый rpc-метод

--- server/report/server.py
+++ server/report/server.py
@@ -1,2 +1,22 @@
+import asyncio
+from aio_pika import connect
+from aio_pika.patterns import RPC
+
+
+def test(*, txt):
+    print(txt)
+    return 'result from printing server'
+
+
+async def main(loop):
+    connection = await connect("amqp://guest:guest@localhost/", loop=loop)
+    channel = await connection.channel()
+    rpc = await RPC.create(channel)
+    await rpc.register('test', test)
+
+
 if __name__ == '__main__':
-    print('I am a reporting server!')
+    loop = asyncio.get_event_loop()
+    loop.create_task(main(loop))
+    print(" [x] Awaiting RPC requests")
+    loop.run_forever()
