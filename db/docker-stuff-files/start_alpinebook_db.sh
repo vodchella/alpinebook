d931738b7f54241be502b0c16379c2475f821280
twister <twister.kz@gmail.com>
2018-01-05 16:25:46 +0600

    Не надо стартовать Postgres под рутом, всё равно не выйдет

--- db/docker-stuff-files/start_alpinebook_db.sh
+++ db/docker-stuff-files/start_alpinebook_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service postgresql start
-python3 /tmp/wait_for_postgres.py
-/tmp/db/db_update.sh
+python3 /etc/alpinebook/wait_for_postgres.py
+/etc/alpinebook/db/db_update.sh
 service postgresql stop
 /usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf
