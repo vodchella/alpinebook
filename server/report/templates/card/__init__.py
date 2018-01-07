889184fd8d92549e0a9b41b78f30a781119e72c9
vodchella <twister.kz@gmail.com>
2018-01-07 20:58:31 +0600

    Реализовал передачу опций в процедуру report.get_data (процедура, возвращающая все данные для генерации отчёта)

--- server/report/templates/card/__init__.py
+++ server/report/templates/card/__init__.py
@@ -15,7 +15,9 @@ class Report(IReportTemplate):
     def get_title(self):
         return 'Учётная карточка альпиниста'
 
-    async def get_data(self):
+    async def get_data(self, **options):
         alpinist_id = self._params['alpinist_id'] if 'alpinist_id' in self._params else 0
         url = f'{HTTP_SERVER_URL}/api/v1/summits/alpinist/{alpinist_id}'
-        return await request(url, self._jwt)
+        rows = await request(url, self._jwt)
+        return {'rows': rows,
+                'server_url': HTTP_SERVER_URL if 'for_pdf' in options and options['for_pdf'] else ''}
