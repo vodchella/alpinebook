e27ebe960a76c1fa009fe887fde90e3860a8dcec
twister <twister.kz@gmail.com>
2018-01-12 00:46:18 +0600

    Обработка ошибок MongoDB

--- server/http/pkg/utils/decorators/handle_exceptions.py
+++ server/http/pkg/utils/decorators/handle_exceptions.py
@@ -2,11 +2,12 @@ from pkg.utils.errors import response_error, get_raised_error
 from pkg.constants.error_codes import (
     ERROR_INTERNAL_EXCEPTION, ERROR_DATABASE_EXCEPTION,
     ERROR_JWT_INVALID_TOKEN, ERROR_JWT_INVALID_KEY,
-    ERROR_RABBITMQ_EXCEPTION
+    ERROR_RABBITMQ_EXCEPTION, ERROR_MONGODB_EXCEPTION
 )
 from asyncpg.exceptions._base import PostgresError
 from jwt.exceptions import InvalidTokenError, InvalidKeyError
 from pika.exceptions import AMQPError
+from pymongo.errors import PyMongoError
 
 
 def handle_exceptions(func):
@@ -21,6 +22,8 @@ def handle_exceptions(func):
             return response_error(ERROR_JWT_INVALID_KEY, get_raised_error())
         except AMQPError:
             return response_error(ERROR_RABBITMQ_EXCEPTION, get_raised_error(), default_logger='rabbitmq')
+        except PyMongoError:
+            return response_error(ERROR_MONGODB_EXCEPTION, get_raised_error(), default_logger='mongodb')
         except:
             return response_error(ERROR_INTERNAL_EXCEPTION, get_raised_error())
     return wrapped
