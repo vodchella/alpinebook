c1c15152f606069568095808173bb3de0c128d72
twister <twister.kz@gmail.com>
2018-01-15 23:13:20 +0600

    Начинаю разработку мобильного приложения

---
+++ frontend/mobile/index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import Alpinebook from './src/app';
+
+AppRegistry.registerComponent('Alpinebook', () => Alpinebook);
---
+++ frontend/mobile/src/app.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Alpinebook extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Добро пожаловать в Alpinebook!
+        </Text>
+        <Text style={styles.instructions}>
+          Это первый коммит приложения, которое просто умеет запускаться :)
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
