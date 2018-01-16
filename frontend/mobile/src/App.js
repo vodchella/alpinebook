3280265d87022dd6b900b8705bfceeb18643e0e4
twister <twister.kz@gmail.com>
2018-01-16 15:08:12 +0600

    Переименовал каталог

--- frontend/mobile/src/App.js
+++ frontend/mobile/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import HomeScreen from './screens/HomeScreen';
-import RoutesScreen from './screens/RoutesScreen';
-import SummitsScreen from './screens/SummitsScreen';
+import HomeScreen from './containers/HomeScreen';
+import RoutesScreen from './containers/RoutesScreen';
+import SummitsScreen from './containers/SummitsScreen';
 import { DrawerNavigator, StackNavigator } from 'react-navigation';
 
 
@@ -9,12 +9,12 @@ import { DrawerNavigator, StackNavigator } from 'react-navigation';
  * DrawerNavigator скрывает заголовки экранов и легально с этим ничего нельзя сделать
  * Поэтому оборачиваем каждый экран в одностраничный StackNavigator, так заголовки видны
  */
-const routesScreenWrapper = StackNavigator (
+const RoutesScreenWrapper = StackNavigator (
     { RoutesScreen: { screen: RoutesScreen } },
     { navigationOptions: ({ navigation }) => ({ initialRouteName: 'RoutesScreen' }) }
 );
 
-const summitsScreenWrapper = StackNavigator (
+const SummitsScreenWrapper = StackNavigator (
     { SummitsScreen: { screen: SummitsScreen } },
     { navigationOptions: ({ navigation }) => ({ initialRouteName: 'SummitsScreen' }) }
 );
@@ -23,8 +23,8 @@ const summitsScreenWrapper = StackNavigator (
 const RootDrawer = DrawerNavigator(
     {
         Home: { screen: HomeScreen },
-        Routes: { screen: routesScreenWrapper },
-        Summits: { screen: summitsScreenWrapper }
+        Routes: { screen: RoutesScreenWrapper },
+        Summits: { screen: SummitsScreenWrapper }
     },
     { initialRouteName: 'Home' }
 );
