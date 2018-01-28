4a49cf31db2db8c8c04a0661be371ffd84b57917
Maxim Pavlov <twister.kz@gmail.com>
2018-01-28 06:59:01 +0600

    Http-ответ с ошибкой теперь имеет код 500. Resolves #11 (#15)

--- backend/server/http/pkg/utils/errors.py
+++ backend/server/http/pkg/utils/errors.py
@@ -15,7 +15,7 @@ def get_raised_error(full=False):
         return (e[-1:][0]).strip('\n')
 
 
-def response_error(code, message, status=200, default_logger='alpinebook', log_stacktrace=True):
+def response_error(code, message, status=500, default_logger='alpinebook', log_stacktrace=True):
     error_json = {'error': {'code': code, 'message': message}}
     stacktrace_log_msg = ''
     if log_stacktrace:
