6199c28937956144bd6739b026e1cb2cc1807794
vodchella <twister.kz@gmail.com>
2018-01-30 23:29:12 +0600

    Запустил под Expo. Не отображаются иконки, Header уехал под системный StatusBar

--- frontend/mobile/index.js
+++ frontend/mobile/index.js
@@ -1,4 +1,6 @@
+import 'expo';
 import { AppRegistry } from 'react-native';
 import Alpinebook from './src/App';
 
 AppRegistry.registerComponent('Alpinebook', () => Alpinebook);
+AppRegistry.registerComponent('main', () => Alpinebook);
