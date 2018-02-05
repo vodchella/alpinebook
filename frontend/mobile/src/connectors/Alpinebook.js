9cf0ab3558232cf13867bf839ea8778459939919
vodchella <twister.kz@gmail.com>
2018-02-06 01:26:16 +0600

    Вставил отладочную заглушку чтобы mockapi-сервер отдавал приемлемую инфу

--- frontend/mobile/src/connectors/Alpinebook.js
+++ frontend/mobile/src/connectors/Alpinebook.js
@@ -21,11 +21,18 @@ class Alpinebook {
     }
 
     searchMountains(query, areaId, onOk, onFail) {
-        const queryStr = areaId ?
-                            `areas/${areaId}/mountains?search=${query}`
-                            :
-                            `mountains?search=${query}`;
-        requestAlpinebook(queryStr, onOk, onFail, (result) => result);
+        if (areaId === 1) {
+            // Это отладочная заглушка
+            // TODO: Убрать после переезда на свой сервер
+            const strForDebug = `areas/5/mountains?search=${query}`;
+            requestAlpinebook(strForDebug, onOk, onFail, (result) => result);
+        } else {
+            const queryStr = areaId ?
+                                `areas/${areaId}/mountains?search=${query}`
+                                :
+                                `mountains?search=${query}`;
+                                requestAlpinebook(queryStr, onOk, onFail, (result) => result);
+        }
     }
 
     getRoutes(mountainId, onOk, onFail) {
