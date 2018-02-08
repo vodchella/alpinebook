01d0f4b4abd75ba97dece6eca78a128b6cb8b37e
twister <twister.kz@gmail.com>
2018-02-08 19:02:30 +0600

    Нарисовал окончательный макет формы поиска

--- frontend/mobile/src/components/MountainSearchList.js
+++ frontend/mobile/src/components/MountainSearchList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react/native';
-import { View, ActivityIndicator } from 'react-native';
-import { Content, Text } from 'native-base';
+import { View, ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { Text, Badge, Icon } from 'native-base';
 import ArrayDataStore from '../stores/ArrayDataStore';
 import SearchResultHint from './SearchResultHint';
 import styles from '../styles/Styles';
@@ -15,6 +15,38 @@ class MountainSearchList extends Component {
     render() {
         const { navigation } = this.props;
         const { query } = navigation.state.params;
+        const testData = [
+            {
+                id: 1,
+                name: 'С запада',
+                complexity: '1Б'
+            },
+            {
+                id: 5,
+                name: 'ЮЗ гребень',
+                complexity: '2А'
+            },
+            {
+                id: 2,
+                name: 'Восточный гребень',
+                complexity: '2Б'
+            },
+            {
+                id: 3,
+                name: 'СЗ ребро',
+                complexity: '2Б'
+            },
+            {
+                id: 4,
+                name: 'Северная стена',
+                complexity: '3Б'
+            },
+            {
+                id: 6,
+                name: 'тр-с до Маншук Маметовой',
+                complexity: '4А'
+            }
+        ];
 
         return (
             <View style={{ flex: 1, flexDirection: 'column' }}>
@@ -24,10 +56,91 @@ class MountainSearchList extends Component {
                             <ActivityIndicator size='large' color='gray' />
                         </View>
                         :
-                        this.store.data.length ?
-                            <Content>
-                                <Text />
-                            </Content>
+                        //this.store.data.length ?
+                        1 === 1 ?
+                            <ScrollView>
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                            flexDirection: 'column',
+                                            alignItems: 'flex-start',
+                                            paddingLeft: 20,
+                                            paddingTop: 10
+                                        }}
+                                    >
+                                        <Text
+                                            style={{
+                                                color: 'grey',
+                                                alignSelf: 'flex-start'
+                                            }}
+                                        >
+                                            Тянь-Шань
+                                        </Text>
+                                        <Text
+                                            style={{
+                                                color: 'grey',
+                                                alignSelf: 'flex-start',
+                                                fontSize: 12,
+                                                paddingLeft: 10
+                                            }}
+                                        >Заилийский алатау</Text>
+                                        <TouchableOpacity
+                                            style={{
+                                                flex: 1,
+                                                flexDirection: 'row',
+                                                alignSelf: 'flex-start',
+                                                paddingLeft: 10,
+                                                paddingTop: 15,
+                                                paddingRight: 15,
+                                                height: 45,
+                                                justifyContent: 'space-between',
+                                                width: '100%'
+                                            }}
+                                        >
+                                            <Text>Амангельды</Text>
+                                            <Icon
+                                                name='arrow-down'
+                                                style={{ color: 'lightgrey', fontSize: 20 }}
+                                            />
+                                        </TouchableOpacity>
+                                        {testData.map((item) =>
+                                            <TouchableOpacity
+                                                key={item.id}
+                                                style={{
+                                                    flex: 1,
+                                                    flexDirection: 'row',
+                                                    alignSelf: 'flex-start',
+                                                    paddingLeft: 10,
+                                                    height: 45,
+                                                    width: '100%'
+                                                }}
+                                            >
+                                                <Badge
+                                                    style={{
+                                                        borderRadius: 5,
+                                                        height: 25,
+                                                        width: 45,
+                                                        alignSelf: 'center',
+                                                        backgroundColor: '#477EEA'
+                                                    }}
+                                                >
+                                                    <Text>
+                                                        {item.complexity}
+                                                        {item.winter_complexity ? '*' : null}
+                                                    </Text>
+                                                </Badge>
+                                                <Text
+                                                    style={{
+                                                        paddingLeft: 20,
+                                                        paddingRight: 50,
+                                                        alignSelf: 'center'
+                                                    }}
+                                                >
+                                                    {item.name}
+                                                </Text>
+                                            </TouchableOpacity>)}
+                                    </View>
+                            </ScrollView>
                             :
                             <View style={styles.container}>
                                 <Text style={styles.inactiveText}>Нет данных</Text>
