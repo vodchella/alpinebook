8b353278147308e7f66b850ebec69d09283d07bc
twister <twister.kz@gmail.com>
2018-02-22 14:38:34 +0600

    Из таблицы "Маршруты" удаляю колонки, связанные с первопрохождением

--- backend/db/v0.01/objects/functions/public/get_route_json.sql
+++ backend/db/v0.01/objects/functions/public/get_route_json.sql
@@ -9,8 +9,6 @@ $BODY$
                            'name', public.get_route_text(r.route_id, false, false),
                            'traverse', r.traverse_bool,
                            'ending_mountain', get_mountain_json(r.ending_mountain_id),
-                           'first_ascender', get_alpinist_json(r.first_ascender_id),
-                           'first_ascent_year', r.first_ascent_year,
                            'description', r.description) as route
   from   routes r
   where  r.route_id = id;
