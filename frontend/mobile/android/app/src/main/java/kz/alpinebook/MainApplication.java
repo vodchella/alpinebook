1897e5b8ba0ca75887e6a293719d773927d7424b
twister <twister.kz@gmail.com>
2018-02-22 10:49:26 +0600

    Устанавливаю react-native-fetch-blob

--- frontend/mobile/android/app/src/main/java/kz/alpinebook/MainApplication.java
+++ frontend/mobile/android/app/src/main/java/kz/alpinebook/MainApplication.java
@@ -3,6 +3,9 @@ package kz.alpinebook;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage(),
+            new RNFetchBlobPackage()
       );
     }
 
